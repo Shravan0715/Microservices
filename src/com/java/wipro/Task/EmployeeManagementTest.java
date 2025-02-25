f7468c2345a35ca0314cef92ee1854e66f6c6785
Shravan0715 <shravangalagali573@gmail.com>
2025-02-25 12:33:01 +0530

    Commit SOLID Task

--- src/com/java/wipro/Task/EmployeeManagementTest.java
+++ src/com/java/wipro/Task/EmployeeManagementTest.java
@@ -5,17 +5,14 @@ import java.util.List;
 
 public class EmployeeManagementTest {
 	public static void main(String[] args) {
-        List<Employee> employees = new ArrayList<>();
+        List<Workable> employees = new ArrayList<>();
 
         employees.add(new Manager("Shravan", 101, 80000, 10));//manager
         employees.add(new Developer("Shivu", 102, 60000, "Java"));//developer
         employees.add(new Developer("Shimba", 105, 50000, "C#"));//developer
         employees.add(new SecurityGuard("Shree", 103, 30000, "Night"));//securityguard
         employees.add(new SecurityGuard("Shivam", 104, 25000, "Morning"));
-        for (Employee emp : employees) {
-            emp.showDetails();
-            emp.performDuties();
-            System.out.println("------------------------------------------");
-        }
+        EmployeeManagementSystem managementSystem = new EmployeeManagementSystem(employees);
+        managementSystem.manageEmployees();
     }
 }
