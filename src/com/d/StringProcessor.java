ebd4d500549b522bbe5a809881e74737181a2847
Shravan0715 <shravangalagali573@gmail.com>
2025-02-26 13:03:28 +0530

    commit task

---
+++ src/com/d/StringProcessor.java
@@ -0,0 +1,19 @@
+package Microservices.src.com.d;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+public class StringProcessor {
+	private final StringReader stringreader;
+	private final StringWriter stringwriter;
+	
+	public StringProcessor(StringReader stringreader, StringWriter stringwriter) {
+		super();
+		this.stringreader = stringreader;
+		this.stringwriter = stringwriter;
+	}
+	
+	public void printString() {
+		stringwriter.write(stringreader.toString());
+	}	
+}
